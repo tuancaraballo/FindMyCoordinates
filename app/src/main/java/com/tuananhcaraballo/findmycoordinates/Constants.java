d5ab3b351af07eb804ca8a38881c0d5aa99a2d40
tuancaraballo <tranc2606@gmail.com>
2016-07-25 13:38:35 -0700

    done: added contants class working: adding a class to fetch info

---
+++ app/src/main/java/com/tuananhcaraballo/findmycoordinates/Constants.java
@@ -0,0 +1,17 @@
+package com.tuananhcaraballo.findmycoordinates;
+
+/**
+ * Created by Antuan on 7/25/16.
+ * Constant values used across the program
+ * Notice how they are static, meaning that you don't have the instantiate an
+ * object of type Constants :-)
+ */
+public class Constants {
+    public static final int SUCCESS_RESULT = 1; // google reference has it as 0
+    public static final int FAILURE_SRESULT = 0;
+    public  static  final String PACKAGE_NAME = "com.tuananhcaraballo.findmycoordinates";
+    public static  final  String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static  final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY ";
+    public  static  final  String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+
+}
