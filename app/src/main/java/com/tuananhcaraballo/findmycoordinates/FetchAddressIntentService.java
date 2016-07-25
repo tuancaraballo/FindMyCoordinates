a12ba48b47403e1f5261b0fe14b9380bfe5dc6b1
tuancaraballo <tranc2606@gmail.com>
2016-07-25 14:57:17 -0700

    Working: try and catch

--- app/src/main/java/com/tuananhcaraballo/findmycoordinates/FetchAddressIntentService.java
+++ app/src/main/java/com/tuananhcaraballo/findmycoordinates/FetchAddressIntentService.java
@@ -2,7 +2,15 @@ package com.tuananhcaraballo.findmycoordinates;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.ResultReceiver;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Antuan on 7/25/16.
@@ -10,7 +18,7 @@ import android.os.ResultReceiver;
 public class FetchAddressIntentService extends IntentService{
 
 
-    private static final String TAG = "FetchAddressIntentService";
+    private static final String TAG = "FetchAddressIS";
     protected ResultReceiver mReceiver; // --> receiver sent by the user as an extra in the intent
 
     public FetchAddressIntentService() { //-->Constructor
@@ -20,6 +28,41 @@ public class FetchAddressIntentService extends IntentService{
     @Override
     protected void onHandleIntent(Intent intent) {
         String errorMessage = "";
-        mReceiver
+        // 1 --> Gets receiver from intent that was passed from MainActivity as an extra
+        mReceiver = intent.getParcelableExtra(Constants.RECEIVER);
+
+        if(mReceiver == null){ //--> some error checking
+            Log.wtf(TAG,"No Receiver was passed");
+            return;
+        }
+        // 2 --> Location that was passed to this activity from MainActivity as an extra
+        Location location = intent.getParcelableExtra(Constants.LOCATION_DATA_EXTRA);
+
+        if(location == null){ // --> some error checking
+            errorMessage = "No location was provided";
+            Log.wtf(TAG,errorMessage);
+            deliverResultToReceiver(Constants.FAILURE_RESULT,errorMessage);
+            return;
+        }
+        // 3 --> Define a geocoder Object
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        // 4 --> Declare a List of type Addresses:
+        List<Address> addresses = null;
+
+        // 5 --> Get the address:
+        try{
+            addresses = geocoder.getFromLocation(location.getLatitude(),
+                    location.getLongitude(),1);
+        }catch (IOException ioExcpetion){ //-->catch network or other I/O problems
+            errorMessage = "Service not available";
+            Log.e(TAG,errorMessage,ioExcpetion);
+        }catch (IllegalArgumentException illegalArgumentException){
+            //-->catches invalid latitdue or longitude values
+        }
+
+
+    }
+
+    private void deliverResultToReceiver(int resultCodet, String errorMessage) {
     }
 }
