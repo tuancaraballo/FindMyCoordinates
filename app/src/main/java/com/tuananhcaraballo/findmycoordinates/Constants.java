a7accd4546e386837f4dd8cdb908b0240ee4b535
tuancaraballo <tranc2606@gmail.com>
2016-08-22 23:30:22 -0700

    -Done saving neccessary variables onSavedInstanceState -Resolve the issue with duplicate markers - The map no longer goes to the default location, but to your current one. - Next: work on getting weather data, you can call an asyncTask onConnected

--- app/src/main/java/com/tuananhcaraballo/findmycoordinates/Constants.java
+++ app/src/main/java/com/tuananhcaraballo/findmycoordinates/Constants.java
@@ -13,5 +13,7 @@ public class Constants {
     public static  final  String RECEIVER = PACKAGE_NAME + ".RECEIVER";
     public static  final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY ";
     public  static  final  String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+    public static final String LATITUDE = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
 
 }
